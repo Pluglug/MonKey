84f4a0c806c64ab27d3c889f9e74b328d649374c
Pluglug_349 <upward571103@gmail.com>
2024-04-03 13:50:58 +0900

    log不在のため一時的にコメントアウト

--- debug/debug_utils.py
+++ debug/debug_utils.py
@@ -1,17 +1,19 @@
-from .vlog import log
-from .debug_flags import DBG_OPS
+# from .vlog import log
 
-import functools
+# from .debug_flags import DBG_OPS
+
+# import functools
 
 def log_exec(func):
-    @functools.wraps(func)
-    def wrapper(self, context, *args, **kwargs):
-        if DBG_OPS:
-            log.header(f"Starting {self.bl_idname} Execution")
-            log.increase()
-        result = func(self, context, *args, **kwargs)
-        if DBG_OPS:
-            log.decrease()
-            log.info(f"Finished {self.bl_idname}")
-        return result
-    return wrapper
+    pass
+    # @functools.wraps(func)
+    # def wrapper(self, context, *args, **kwargs):
+    #     if DBG_OPS:
+    #         log.header(f"Starting {self.bl_idname} Execution")
+    #         log.increase()
+    #     result = func(self, context, *args, **kwargs)
+    #     if DBG_OPS:
+    #         log.decrease()
+    #         log.info(f"Finished {self.bl_idname}")
+    #     return result
+    # return wrapper
